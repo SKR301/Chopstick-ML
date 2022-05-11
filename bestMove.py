0f5625b4c83d2a316fc845dd301eba0a780ed7b2
SKR301 <chunnu.bihat@gmail.com>
2022-05-11 21:56:45 +0530

    added best next move predictor

---
+++ bestMove.py
@@ -0,0 +1,11 @@
+from model import modelData
+import time
+
+if __name__ == '__main__':
+    while True:
+        state = input('Enter state:')
+        possibleMoves = modelData[state]
+        sortedMoves = dict(sorted(possibleMoves.items(), key=lambda item: item[1]))
+        nextMove = list(sortedMoves.keys())[-1]
+        print(f'Next best move from {state} is {nextMove}')
+        time.sleep(1)
