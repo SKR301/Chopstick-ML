3bb6c575870409111737968134b790b3c2d616f4
SKR301 <chunnu.bihat@gmail.com>
2022-05-13 13:16:13 +0530

    updated data

--- bestMove.py
+++ bestMove.py
@@ -3,7 +3,7 @@ import time
 
 if __name__ == '__main__':
     while True:
-        state = input('Enter state:')
+        state = input('Enter state: ')
         possibleMoves = modelData[state]
         sortedMoves = dict(sorted(possibleMoves.items(), key=lambda item: item[1]))
         nextMove = list(sortedMoves.keys())[-1]
