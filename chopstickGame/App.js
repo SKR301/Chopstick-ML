fabb83105a8fb27b835ddf47a6e81b16cdc9e30c
SKR301 <chunnu.bihat@gmail.com>
2022-05-26 20:57:08 +0530

    #4 sort with maintaining ordder

--- chopstickGame/App.js
+++ chopstickGame/App.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { stateData100 } from './assets/modelCollection';
+import { stateData10000 } from './assets/modelCollection';
+import { stateData10000000 } from './assets/modelCollection';
 
 export default function App() {
 	const [state, setState] = useState('1111');
@@ -84,12 +86,20 @@ export default function App() {
 	const playComputerTurn = () => {
 		let optimalState01 = state.substring(0,2);
 		let optimalState23 = state.substring(2,4);
+		let swap = [false, false];
+		
+		let temp1 = optimalState01.split('').sort().reverse().join('');
+		let temp2 = optimalState23.split('').sort().reverse().join('');
+		swap[0] = !(optimalState01[0] > optimalState01[1]);
+		swap[1] = !(optimalState23[0] > optimalState23[1]);
 
-		// Need to sort but maintain order 
-		let optimalState = optimalState01.split('').sort().reverse().join('') + optimalState23.split('').sort().reverse().join('');
+		let optimalState = temp1 + temp2;
 
-		let nextState = sortDict(stateData100[optimalState])[0][0];
-		let properState = nextState.substring(2,4)+nextState.substring(0,2);
+		let nextState = sortDict(stateData10000000[optimalState])[0][0];
+		temp1 = (swap[0])? nextState.substring(2,4).split('').reverse().join(''): nextState.substring(2,4);
+		temp2 = (swap[1])? nextState.substring(0,2).split('').reverse().join(''): nextState.substring(0,2);
+		let properState = temp1 + temp2;
+
 		setTimeout(()=>{
 			setState(properState);
 		},500); 
