691b7b8d2dd434de90a7add051436d6788a9b06f
SKR301 <chunnu.bihat@gmail.com>
2022-07-11 12:02:46 +0530

    #7 more trained data added(master data)

--- validateModel/main.py
+++ validateModel/main.py
@@ -4,13 +4,7 @@ Also normalize it first.
 Robust model: There is only one best solution from any given stage.
 """
 
-from modelCollection import stateData10000000
-from modelCollection import stateData1000000
-from modelCollection import stateData100000
-from modelCollection import stateData10000
-from modelCollection import stateData1000
-from modelCollection import stateData100
-
+from data import data
 
 def checkRobustness(states):
     normalizedData = {}
@@ -24,6 +18,7 @@ def checkRobustness(states):
         sortedMoves = dict(sorted(value.items(), key=lambda item: item[1]))
         normalizedData[key] = sortedMoves
         
+    # print(normalizedData)
 
     thresholds = [0.5, 0.75, 0.9]
 
@@ -38,21 +33,6 @@ def checkRobustness(states):
 
 
 if __name__ == '__main__':
-    print('\t100')
-    checkRobustness(stateData100)
-    print()
-    print('\t1000')
-    checkRobustness(stateData1000)
-    print()
-    print('\t10000')
-    checkRobustness(stateData10000)
-    print()
-    print('\t100000')
-    checkRobustness(stateData100000)
-    print()
-    print('\t1000000')
-    checkRobustness(stateData1000000)
-    print()
-    print('\t10000000')
-    checkRobustness(stateData10000000)
+    # print('\t100')
+    checkRobustness(data)
     print()
